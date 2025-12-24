1435dcd165a395ef12306c885a3cb70e6b9f3242
Victor Enache <enachevictor887@gmail.com>
2025-12-24 11:11:10 +0200

    feat: added main top bar

--- app/src/main/java/com/victorenache/cryptotracker/presentation/all_coins_screen/AllCoinsScreen.kt
+++ app/src/main/java/com/victorenache/cryptotracker/presentation/all_coins_screen/AllCoinsScreen.kt
@@ -1,11 +1,16 @@
 package com.victorenache.cryptotracker.presentation.all_coins_screen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -14,14 +19,28 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.victorenache.cryptotracker.presentation.components.AllCoinsItem
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemColors
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.victorenache.cryptotracker.presentation.components.MainTopBar
 import com.victorenache.cryptotracker.ui.theme.BlackScreen
 import com.victorenache.cryptotracker.ui.theme.WhiteDivider
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -30,40 +49,97 @@ fun AllCoinsScreen(
     viewModel: AllCoinsViewModel = hiltViewModel()
 ){
     val state = viewModel.state.value
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Scaffold(modifier = Modifier.fillMaxSize(),
-            containerColor = BlackScreen,
-            contentWindowInsets = WindowInsets(0.dp)
-    ) {paddingValues ->
-        PullToRefreshBox(
-            isRefreshing = state.isRefreshing,
-            onRefresh = {viewModel.refreshCoins() },
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        )
-        {
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier.width(320.dp),
+                drawerContainerColor = Color(0xFF1A1A1A),
+                drawerContentColor = Color.White
+            ) {
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    "CRYPTO MENU",
+                    modifier = Modifier.padding(16.dp)
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.ExtraBold,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                HorizontalDivider(color = Color.Gray, thickness = 0.5.dp)
 
-            LazyColumn(modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(top = 40.dp, bottom = 40.dp)) {
-
-                item {
-                    HorizontalDivider(
-                        thickness = 0.7.dp,
-                        color = WhiteDivider
+                NavigationDrawerItem(
+                    label = { Text(text = "Market Live") },
+                    selected = true,
+                    onClick = {
+                        scope.launch { drawerState.close() }
+                    },
+                    colors = NavigationDrawerItemDefaults.colors(
+                        unselectedContainerColor = Color.Transparent,
+                        selectedContainerColor = Color(0xFF333333),
                     )
-                }
-                items(state.coins)
-                { coin ->
-                    AllCoinsItem(coin = coin)
-                    HorizontalDivider(
-                        thickness = 0.7.dp,
-                        color = WhiteDivider
+                )
+                NavigationDrawerItem(
+                    label = { Text("Favorites") },
+                    selected = false,
+                    onClick = { },
+                    colors = NavigationDrawerItemDefaults.colors(
+                        unselectedContainerColor = Color.Transparent,
+                        selectedContainerColor = Color(0xFF333333)
                     )
-                }
+                )
             }
-            if(state.isLoading) {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            containerColor = BlackScreen,
+            topBar = {
+                MainTopBar(
+                    title = "CRYPTO TRACKER",
+                    onMenuClick = {
+                        scope.launch { drawerState.open() }
+                    },
+                    onSearchClick = {
+
+                    }
+                )
+            },
+            contentWindowInsets = WindowInsets(0.dp)
+        ) {paddingValues ->
+            PullToRefreshBox(
+                isRefreshing = state.isRefreshing,
+                onRefresh = {viewModel.refreshCoins() },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            )
+            {
+
+                LazyColumn(modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(bottom = 40.dp)) {
+
+                    item {
+                        HorizontalDivider(
+                            thickness = 0.7.dp,
+                            color = WhiteDivider
+                        )
+                    }
+                    items(state.coins)
+                    { coin ->
+                        AllCoinsItem(coin = coin)
+                        HorizontalDivider(
+                            thickness = 0.7.dp,
+                            color = WhiteDivider
+                        )
+                    }
+                }
+                if(state.isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
             }
         }
     }
---
+++ app/src/main/java/com/victorenache/cryptotracker/presentation/components/MainTopBar.kt
@@ -0,0 +1,60 @@
+package com.victorenache.cryptotracker.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.victorenache.cryptotracker.ui.theme.BlackScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(
+    title: String,
+    onMenuClick: () -> Unit,
+    onSearchClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onMenuClick) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.White
+
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onSearchClick) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search",
+                    tint = Color.White
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = BlackScreen,
+            scrolledContainerColor = BlackScreen
+        )
+    )
+}
